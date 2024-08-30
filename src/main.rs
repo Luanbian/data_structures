7e85d45da68bb736e926adbc8646ab8628ae9f3d
Luan Almeida <luan.almeida@jbtec.com.br>
2024-08-29 23:47:58 -0300

    init references

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 10;
+    let b;
+    let c;
+    b = 20;
+    c = a + b;
+    println!("{}", c); // mostrando valor
+    println!("{:p}", &c); // mostrando endereço na memória
 }
